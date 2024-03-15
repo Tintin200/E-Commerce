b0b20e7248b735f85246ff64644ecf59c9ab0c72
Ayman Mougou <aymoug@outlook.fr>
2024-03-15 10:36:26 +0100

    Part1

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BaseController extends AbstractController
+{
+    #[Route('/', name: 'app_accueil')]
+    public function index(): Response
+    {
+        return $this->render('base/index.html.twig', [
+
+        ]);
+    }
+
+    #[Route('/box', name: 'app_box')]
+    public function box(): Response
+    {
+        return $this->render('base/box.html.twig', [
+
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('base/about.html.twig', [
+
+        ]);
+    }
+}
